00acbdd27e9ad60a8787fb80c371cb3bd94277d6
VexShura <345659@student.mboutrecht.nl>
2026-09-23 02:28:15 -0700

    opdracht 1 afgemaakt

--- week-01/opdracht1.py
+++ week-01/opdracht1.py
@@ -1,28 +1,32 @@
 # Oefening 1
 # Print de volgende zin "Hello World"
 
-print()
+print(Hello World)
 
 
 # Oefening 2
 # Verander de waarde van de onderstaande variabelen.
 # Print deze daarna 1 voor 1 uit
 
-naam = ""
-leeftijd = 0
-woonstad = ""
+naam = "Yigit"
+leeftijd = 18
+woonstad = "Isselstein"
 
+print("Hallo mijn naam is Yigit")
 
 # Oefening 3
 # Gebruik nu bovenstaande variabelen om zinnen te bouwen
 # Bijvoorbeeld print("Hallo mijn naam is ", naam) of print(f"Mijn naam is {naam}")
 
-
+print(Mijn favoriete game is Cold war Ik heb hier ongeveer 1000 uren op, En ik geef deze game een 9) 
 
 # Oefening 4
 # Maak variabelen aan voor je favoriete game, hoe veel uur je deze hebt gespeeld en welk cijfer je dit spel zou geven
 # Print deze daarna in zinnen uit, bijvoorbeeld "Mijn favoriete game is Minecraft" "Ik heb deze game 150 uur gespeeld", "Ik geef deze game een 8.5"
 
+number1 = 10 
+
+number2 = 5
 
 
 # Oefening 5
@@ -30,21 +34,30 @@ woonstad = ""
 # Bereken daarna de som (+), het verschil (-) en het product (*) uit van deze nummers.
 # Print daarna de uitkomsten uit
 
-
+print("de som van number1 en number2 is: ", 10 + 5)
+print("het verschil van number1 en number2 is: ", 10 - 5)
+print("het product van number1 en number2 is: ", 10 * 5)
 
 # Oefening 6
 # Maak een simpel game character met minimaal de volgende variabelen: name, health, level, damage
 # Print deze vervolgens uit
 # Zorg er daarna voor dat je character 20 damage neemt, print nu de nieuwe waarde van zijn health uit
 
-
+print("Naam: {Vex}")
+print("Health: {150}")
+print("Level: {1}")
+print("Damage: {25}")
 
 # Oefening 7
 # Ga verder met je character van de vorige oefening. Voeg nu een nieuw variabel "weapon" toe.
 # Geef het wapen een naam, verhoog de damage van je character en verhoog het level met 1
 # Print daarna de nieuwe waardes uit 
 
-
+print("Naam: {Vex}")
+print("Health: {160}")
+print("Level: {2}")
+print("Damage: {45}")
+print("Wapen: {Sword}")
 
 # Oefening 8
 # Maak een programma dat een profiel van een gamer laat zien
@@ -52,3 +65,13 @@ woonstad = ""
 # Print al deze informatie netjes uit
 # Verhoog daarna de score van het profiel met 250 en print de nieuwe waarde
 # Bonus! Voeg zelf 3 nieuwe variabelen toe
+
+print("Naam: {Vex}")
+print("Leeftijd: {18}")
+print("Favoriete game: {Cold war}")
+print("Uren gespeeld: {1000}")
+print("Level: {100 Prestige 5}")
+print("Score: {99999}")
+print("Rank: {Champ}")
+print("Land: {Netherlands}")
+print("Online: {isOnline}")
